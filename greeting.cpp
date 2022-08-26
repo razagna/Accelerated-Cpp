22dbcd5679f0bcf92e2ea8004e135150cd488398
Razanleo <razan.i.omer@gmail.com>
2022-08-26 15:51:26 +0200

    initial commit

---
+++ greeting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter your name: ";
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
