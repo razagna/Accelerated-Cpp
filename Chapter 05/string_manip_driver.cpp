299b550b1780043e50a52ca46e678851fb4a70e1
Razanleo <razan.i.omer@gmail.com>
2022-09-12 23:17:16 +0200

    add ch05 framing example

--- Chapter 05/string_manip_driver.cpp
+++ Chapter 05/string_manip_driver.cpp
@@ -18,9 +18,41 @@ void split_test()
     }
 }
 
+vector<string> frame(const vector<string>& vec)
+{
+    vector<string> result;
+
+    // find longest word
+    string::size_type maxlen = 0;
+    for(vector<string>::size_type i = 0; i != vec.size(); ++i)
+        maxlen = max(maxlen, vec[i].size());
+
+    // add top border = longest + 4
+    string border(maxlen + 4, '*');
+    result.push_back(border);
+
+    // find difference between longest and current
+    for(vector<string>::size_type i = 0; i != vec.size(); ++i)
+        result.push_back("* " + vec[i] + string(maxlen - vec[i].size(), ' ') + " *");
+
+    // add bottom border
+    result.push_back(border);
+
+    return result;
+}
+
+void frame_test()
+{
+    vector<string> res = frame({"this is an", "example", "to", "illustrate", "framing"});
+    for(vector<string>::size_type i = 0; i != res.size(); ++i)
+        cout << res[i] << endl;
+}
+
 int main()
 {
-    split_test();
+    //split_test();
+    
+    frame_test();
 
     return 0;
 }
