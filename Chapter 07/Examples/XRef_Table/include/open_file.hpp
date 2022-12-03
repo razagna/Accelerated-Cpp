a5c31980fd574c006c3355639de1d0834cca224e
Razanleo <razan.i.omer@gmail.com>
2022-12-03 13:43:56 +0900

    refactor xref example

--- Chapter 07/Examples/XRef_Table/include/open_file.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef GUARD_open_fie_h
-#define GUARD_open_fie_h
-#include <fstream>
-
-std::ifstream& open_file(std::ifstream&);
-
-#endif
--- Chapter 07/Examples/XRef_Table/xref_default_driver.cpp
+++ Chapter 07/Examples/XRef_Table/xref_default_driver.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <map>
-#include "open_file.hpp"
 #include "xref.hpp"
 using namespace std;
 
 int main()
 {
-    try
+    ifstream input_file("../data/sample.txt");
+    map<string, vector<int> > ret = xref(input_file);
+
+    for (map<string, vector<int> >::const_iterator it = ret.begin(); it != ret.end(); ++it) 
     {
-        ifstream input_file("..\\data\\sample.txt");
-        map<string, vector<int> > res = xref(open_file(input_file));
-        for(map<string, vector<int> >::const_iterator iter = res.begin();
-		    iter != res.end(); ++iter)
-		{
-            vector<int>::const_iterator v_iter = iter->second.begin();   
-            cout << iter->first << "\t" << *v_iter++;
-            while(v_iter != iter->second.end())
-                cout << ", " << *v_iter++;
-            cout << endl;
-        }
+        vector<int>::const_iterator line_it = it->second.begin();
+        cout << it->first << " occurs on line(s): " << *line_it++;
+
+        while (line_it != it->second.end())
+            cout << ", " << *line_it++;
+
+        cout << endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
+    return 0;
 }
