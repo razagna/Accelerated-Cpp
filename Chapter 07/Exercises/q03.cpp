f70983535c51624af77ddad31e1c45cfedb8c133
Razanleo <razan.i.omer@gmail.com>
2022-12-03 14:41:23 +0900

    Refactoring Q7.3

--- Chapter 07/Exercises/q03.cpp
+++ Chapter 07/Exercises/q03.cpp
@@ -13,16 +13,12 @@ int main()
 
     for (map<string, vector<int> >::const_iterator it = ret.begin(); it != ret.end(); ++it) 
     {
-        cout << it->first << " occurs on line(s): ";
         vector<int>::const_iterator line_it = it->second.begin();
-        cout << *line_it;
-        ++line_it;
+        cout << it->first << " occurs on line(s): " << *line_it++;
 
-        while (line_it != it->second.end()) 
-        {
-            cout << ", " << *line_it;
-            ++line_it;
-        }
+        while (line_it != it->second.end())
+            cout << ", " << *line_it++;
+
         cout << endl;
     }
     return 0;
